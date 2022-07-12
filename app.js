4b765177423d625f9545f17da7c3468fa5dbb2ff
gary <gary.white@lodgit.com>
2022-07-12 13:17:59 +0200

    code

---
+++ app.js
@@ -0,0 +1,9 @@
+function reverse(string) {
+    //your code here
+}
+
+console.log(reverse('I am an expert at this')); // 'this at expert an am I'
+console.log(reverse('This is so easy')); // 'easy so is This'
+console.log(reverse('no one cares')); // 'cares one no'
+console.log(reverse('')); // ''
+console.log(reverse('CodeWars')); // 'CodeWars'
